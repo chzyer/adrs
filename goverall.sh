54131251ca899c884af68e7819b7ef4a25bdf926
Cheney <me@chenye.org>
2015-04-11 21:02:41 +0800

    update goverall.sh

--- goverall.sh
+++ goverall.sh
@@ -3,9 +3,13 @@ set -e
 
 # usage: ./goverall.sh [func|html]
 # default: generate cover.out
+output_null=""
+if [[ "$1" != "" ]]; then
+	output_null=">/dev/null"
+fi
 
 mkdir -p .cover
-go list ./... | xargs -I% bash -c 'name="%"; go test % --coverprofile=.cover/${name//\//_} >/dev/null'
+go list ./... | xargs -I% bash -c 'name="%"; go test % --coverprofile=.cover/${name//\//_} '$output_null
 echo "mode: set" > cover.out
 cat .cover/* | grep -v mode >> cover.out
 rm -r .cover
